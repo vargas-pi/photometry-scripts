fix constrained detrending, make normalization optional

analysis_pipeline.py
@@ -12,7 +12,6 @@ from pathlib import Path
 from copy import deepcopy
 from scipy.io import savemat
 from scipy import signal
-from pandas import IndexSlice as idx
 import seaborn as sns
 
 
@@ -134,7 +133,7 @@ class analysis:
     multi_index = pd.MultiIndex(levels = [[]]*3, codes = [[]]*3, names = ('cond','mouse','trial'))
 
     def __init__(self, norm_method, t_endrec, ds_freq = 1, t_prestim=300, ex=5, 
-                 detrend = False, detrend_method = detrend_405_constrained, 
+                 detrend = False, norm = True, detrend_method = detrend_405_constrained, 
                  file_format='npy', file_loc='analyses',fname=None):
         """
         Parameters
@@ -162,6 +161,7 @@ class analysis:
         self.norm_method = norm_method
         self.t_endrec = t_endrec
         self.detrend = detrend
+        self.norm = norm
         self.detrend_method = detrend_method
         self.normed_data = pd.Series([], index = analysis.multi_index, dtype = object)
         self.raw_data = pd.Series([], index = analysis.multi_index, dtype = object)
@@ -259,7 +259,8 @@ class analysis:
         m.resample(self.t)
         if self.detrend: 
             m.F490, m.F405 = self.detrend_method(m)
-        m.F490, m.F405 = self.norm_method(m)
+        if self.norm:
+            m.F490, m.F405 = self.norm_method(m)
 
         #plot the data
         if plot:
@@ -444,7 +445,7 @@ class analysis:
             self.compute()
 
 
-    def plot_both(self, cond=None, c490='g', c405='r', ylim = None,
+    def plot_both(self, cond=None, c490='g', c405='r', ylim = None, linewidth=.5,
                   show=True, ax=None, alpha=.3, figsize=(12,5), scale = 1):
         """
         plot the average normalized 490 and 405 signal with error
@@ -459,10 +460,10 @@ class analysis:
             if ax is None: _,ax=py.subplots(1,1)
             ax.fill_between(self.t, scale*(self.mean_405[cond] + self.err_405[cond]),
                             scale*(self.mean_405[cond] - self.err_405[cond]), color=c405,alpha=alpha  )
-            ax.plot(self.t, scale*self.mean_405[cond], color=c405, linewidth=.5)
+            ax.plot(self.t, scale*self.mean_405[cond], color=c405, linewidth=linewidth)
             ax.fill_between(self.t, scale*self.mean_490[cond] + scale*self.err_490[cond],
                             scale*(self.mean_490[cond] - self.err_490[cond]), color=c490,alpha=alpha )
-            ax.plot(self.t, scale*self.mean_490[cond] , color=c490, linewidth=.5)
+            ax.plot(self.t, scale*self.mean_490[cond] , color=c490, linewidth=linewidth)
             if scale==100:
                 ax.set_ylabel(r'$\frac{\Delta F}{F}$ (%)')
             else:
@@ -483,14 +484,14 @@ class analysis:
                     ax.flatten()[j].fill_between(self.t, scale * (self.mean_405[i] + self.err_405[i]),
                                                  scale * (self.mean_405[i] - self.err_405[i]), 
                                                  color = c405, alpha = alpha  )
-                    ax.flatten()[j].plot(self.t, scale * self.mean_405[i], color = c405, linewidth=.5)
+                    ax.flatten()[j].plot(self.t, scale * self.mean_405[i], color = c405, linewidth=linewidth)
                     bnds.append(ax.flatten()[j].get_ylim())
                 
                 for j,i in enumerate(self.conds):
                     ax.flatten()[j].fill_between(self.t, scale * self.mean_490[i] + scale*self.err_490[i],
                                                  scale * (self.mean_490[i] - self.err_490[i]), 
                                                  color = c490, alpha = alpha)
-                    ax.flatten()[j].plot(self.t, scale * self.mean_490[i] , color=c490, linewidth=.5)
+                    ax.flatten()[j].plot(self.t, scale * self.mean_490[i] , color=c490, linewidth=linewidth)
                     bnds.append(ax.flatten()[j].get_ylim())
                     
                     ax.flatten()[j].set_title(i)
@@ -530,16 +531,16 @@ class analysis:
         cm405=sns.color_palette(cm405,self.conds.size)
         
         if cond is not None:
-            ax.plot(scale * self.all_490.loc[:,idx[cond,mouse]] , 
+            ax.plot(scale * self.all_490.loc[:,pd.IndexSlice[cond,mouse]] , 
                     color = c490, linewidth = linewidth, label= cond)
             if plot_405:
-                ax.plot(scale * self.all_405.loc[:,idx[cond,mouse]], 
+                ax.plot(scale * self.all_405.loc[:,pd.IndexSlice[cond,mouse]], 
                         color = c405, linewidth = linewidth)
             ax.legend() #added
         else:
-            d = scale * self.all_490.loc[:, idx[:, mouse]]
+            d = scale * self.all_490.loc[:, pd.IndexSlice[:, mouse]]
             d.columns = d.columns.get_level_values('cond')
-            d5 = scale * self.all_405.loc[:, idx[:,mouse]]
+            d5 = scale * self.all_405.loc[:, pd.IndexSlice[:,mouse]]
             d5.columns = d.columns.get_level_values('cond')
 
             if plot_405:
@@ -607,7 +608,7 @@ class analysis:
         return ax
 
 
-    def bin_plot(self, binsize, save=False, cond=None, show=True, ax=None, cm='Set2', color=None, scale=1, ylim = None):
+    def bin_plot(self, binsize, save=False, cond=None, show=True, ax=None, cm='Set2', color=None, scale=1, ylim = None, capsize=3, linewidth=1):
         """
         run bin_plot and then plot the data
 
@@ -633,7 +634,7 @@ class analysis:
 
         if cond is not None:
             color=cm[cond] if color is None else color
-            ax.errorbar(x=df.index,y=scale*df['mean'][cond],yerr=scale*df['sem'][cond],color=color, capsize=3, label=cond)
+            ax.errorbar(x=df.index,y=scale*df['mean'][cond],yerr=scale*df['sem'][cond],color=color, capsize=capsize, label=cond, linewidth=linewidth)
             ax.legend()
         else:
             for i in self.conds:
@@ -967,7 +968,7 @@ class analysis:
             return
         
         if filtered:
-            butter=signal.butter(3, 0.002, btype='lowpass', fs=1) #(order, cutoff freq=0.002)
+            butter=signal.butter(3, 0.002, btype='lowpass', fs=self.ds_freq) #(order, cutoff freq=0.002)
             data={}
             for i in self.all_490.columns:
                 data.update({i:signal.filtfilt(*butter,self.all_490[i].fillna(method='ffill'), axis=0)})
@@ -1028,7 +1029,7 @@ class loaded_analysis(analysis):
             else:
                 setattr(self,key,d[key])
 
-def load_analysis(fpath):
+def load_analysis(fpath, recompute = True):
     """
     load an exported analysis (whether json or npy)
 
@@ -1061,6 +1062,8 @@ def load_analysis(fpath):
         a.detrend_method = detrend_405_constrained
     if not hasattr(a, 'norm_method'):
         a.norm_method = a._norm_method
+    if not hasattr(a, 'norm'):
+        a.norm = True
 
     if not isinstance(a.raw_data, pd.Series):
         tmp =  pd.Series([], index = analysis.multi_index, dtype = object)
@@ -1086,6 +1089,7 @@ def load_analysis(fpath):
                 x.trial=max(mi)+1
             tmp[x.cond, x.mouse_id, x.trial] = x
         a.excluded_raw = tmp
-
-    a.compute()
+    
+    if recompute:
+        a.compute()
     return a

extras/import_lick_phot.py
@@ -55,8 +55,8 @@ def open_save_lick(fpath, mouse1 = None, mouse2 = None, cond1 = 0, cond2 = 0, ch
                         t_start = tstart,
                         t_stim = (licks[mouse1].loc[0,'datetime'] - tstart).total_seconds(),
                         cond = cond1)
-        m.left_licks = licks[mouse1].loc[licks[mouse1]['left lick count'].astype(bool),'time offset (ms)'].values
-        m.right_licks = licks[mouse1].loc[licks[mouse1]['right lick count'].astype(bool),'time offset (ms)'].values
+        m.events = {'left_licks':  licks[mouse1].loc[licks[mouse1]['left lick count'].astype(bool),'time offset (ms)'].values/1000.,
+                    'right_licks': licks[mouse1].loc[licks[mouse1]['right lick count'].astype(bool),'time offset (ms)'].values/1000.}
         data.append(m)
     
     if mouse2:
@@ -67,8 +67,8 @@ def open_save_lick(fpath, mouse1 = None, mouse2 = None, cond1 = 0, cond2 = 0, ch
                         t_start = tstart,
                         t_stim = (licks[mouse2].loc[0,'datetime'] - tstart).total_seconds(),
                         cond = cond2)
-        m.left_licks = licks[mouse2].loc[licks[mouse2]['left lick count'].astype(bool),'time offset (ms)'].values
-        m.right_licks = licks[mouse2].loc[licks[mouse2]['right lick count'].astype(bool),'time offset (ms)'].values
+        m.events = {'left_licks':  licks[mouse2].loc[licks[mouse2]['left lick count'].astype(bool),'time offset (ms)'].values/1000.,
+                    'right_licks': licks[mouse2].loc[licks[mouse2]['right lick count'].astype(bool),'time offset (ms)'].values/1000.}
         data.append(m)
 
     np.save(block/'exported_data.npy', data)

utilities.py
@@ -116,19 +116,16 @@ def fit_405(data, constrained):
     #TODO: change this to a butterworth filter instead
     filt_ds490 = gaussian_filter1d(data.F490[::int(data.fs)], sigma = 10)
     filt_ds405 = gaussian_filter1d(data.F405[::int(data.fs)], sigma = 10)
+    
+    m, b = np.polyfit(filt_ds405, filt_ds490, 1)
+    f0_ds = m * filt_ds405 + b
 
     #fit the 405 data to the 490
     if constrained:
-        #try:
-        (m, b), _ = curve_fit(lambda x, m, b: m*x + b, filt_ds405, filt_ds490, bounds=(0,np.inf))
-        # except RuntimeError:
-        #     m=0
-        #     b=filt_ds490.mean()
-        #     print(data.mouse_id)
-        f0_ds = m * filt_ds405 + b
-    else:
-        m, b = np.polyfit(filt_ds405, filt_ds490, 1)
-        f0_ds = m * filt_ds405 + b
+        if m<0:
+            f0_ds = np.zeros_like(filt_ds490)
+            m = None
+            b = None
     
     # upsample the baseline estimate
     f0_fn = interp1d( data.t[::int(data.fs)], f0_ds, 
@@ -164,14 +161,17 @@ def detrend_405_constrained(data:mouse_data):
     """
 
     f0, m, b= fit_405(data, constrained = True)
-    f0 -= np.median(f0[data.t<0])
+    if m is not None:
+        f0 -= np.median(f0[data.t<0])
 
-    normed_490 = data.F490 - f0
-    #map 405 to 490 and subtract the baseline
-    normed_405 = (data.F405*np.abs(m) + b) - f0
-    #map back to 405 space for further processing
-    normed_405 = (normed_405 - b)/np.abs(m)
-    return normed_490, normed_405
+        normed_490 = data.F490 - f0
+        #map 405 to 490 and subtract the baseline
+        normed_405 = (data.F405*np.abs(m) + b) - f0
+        #map back to 405 space for further processing
+        normed_405 = (normed_405 - b)/np.abs(m)
+        return normed_490, normed_405
+    else:
+        return data.F490, data.F405
 
 
 def detrend_405(data:mouse_data):